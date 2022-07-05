78d0d0053e4dd1204899d7606e351ad09e4663fb
Пользователь <murodjontohirov2701@gmail.com>
2022-07-05 12:28:14 +0500

    if01

--- bin/if01.dart
+++ bin/if01.dart
@@ -8,5 +8,20 @@ Args:
 Returns:
     int: return answer.
 */
+int func(int a, int b, int c) {
+  int ans = 0;
+  if (a > b && a > c) {
+    ans = a;
+  }
+  if (b > a && b > c) {
+    ans = b;
+  }
+  if (c > a && c > b) {
+    ans = c;
+  }
+  return ans;
+}
+
 void main() {
+  print(func(6, 4, 8));
 }
